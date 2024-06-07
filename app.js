ad6e06b78dec9ebaae44a4d22f6e42ef00aea413
Kashyap13610 <kashyap.shreesoftic@gmail.com>
2024-06-07 05:47:39 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/.well-known/apple-app-site-association', (req, res) => {
+    const filePath = path.join(__dirname, 'apple-app-site-association.json');
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
